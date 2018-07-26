5e7717cb3f1d86c377104f4ace67102e3b7312ba
Grayson <info@fromconcentratesoftware.com>
2018-07-25 21:34:31 -0400

    Initial Commit

---
+++ Planoker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Planoker
+//
+//  Created by Grayson Hansard on 7/25/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
